cf5d771e62bc04471303b13da675cb4a25049e43
Radim Pernický <radimpernicky@Radim--Mac-mini.local>
2026-04-28 11:34:36 +0200

    Oprava PWA ikon pro Mac a iOS, verze cache v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'detext-v1';
+const CACHE_NAME = 'detext-v2';
 const ASSETS = [
     './',
     './index.html',
@@ -14,6 +14,18 @@ const ASSETS = [
 self.addEventListener('install', (e) => {
     e.waitUntil(
         caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE_NAME) {
+                    return caches.delete(key);
+                }
+            }));
+        })
     );
 });
 
